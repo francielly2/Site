e4a601f9eeed3fc7e72fee136d5bcb63e7946a15
fran <francielly2403@gmail.com>
2025-11-18 12:25:13 -0300

    Update scriptsite.js

--- scriptsite.js
+++ scriptsite.js
@@ -1,21 +1,35 @@
     // Formulário de pagamento
-    const form = document.getElementById("form-pagamento");
-    if (form) {
-        form.addEventListener("submit", e => {
-            e.preventDefault();
-            const cor = document.getElementById("cor").value;
-            const pagamento = document.getElementById("pagamento").value;
-            salvarCompra(cor, pagamento);
-            alert("Compra realizada com sucesso!");
-            listarCompras();
-        });
-    }
+const carrinhoButton = document.getElementById("carrinho-button");
+if (carrinhoButton) {
+    carrinhoButton.addEventListener("click", () => {
+        window.location.href = "Index2.html";
+    });
+}
 
-    // Botão de voltar
-    const btnVoltar = document.getElementById("btn-voltar");
-    if (btnVoltar) {
-        btnVoltar.addEventListener("click", () => {
-            window.location.href = "index.html";
-        });
-    }
-   // 
+const form = document.getElementById("form-pagamento");
+if (form) {
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        const cor = document.getElementById("cor").value;
+        const pagamento = document.getElementById("pagamento").value;
+        salvarCompra(cor, pagamento);
+        alert("Compra realizada com sucesso!");
+        listarCompras();
+    });
+}
+
+// Botão de voltar
+const btnVoltar = document.getElementById("btn-voltar");
+if (btnVoltar) {
+    btnVoltar.addEventListener("click", () => {
+        window.location.href = "index.html";
+    });
+}
+// 
+
+const myCarouselElement = document.querySelector('#carouselExampleCaptions')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: 2000,
+    touch: false
+})
